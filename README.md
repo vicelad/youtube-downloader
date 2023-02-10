3b3f3dbfd005ca36e707360faf6a76ec8e897d51
vicelad <vicelad@gmail.com>
2023-02-10 12:44:47 -0600

    coded functions. added disclaimer and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+A simple YouTube video downloader written in Python, please read the disclaimer before suing me.
+
+WARNING! Some YouTube videos have copyright restrictions.
+Downloading copyrighted videos without permission is a criminal act.
+You are prohibited for any illegal use of this app.
+Always check for video copyright and YouTube's Terms of Service.
+
+Please don't sue me :)
+
+Installation:
--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from pytube import YouTube
 from tkinter import *
+from tkinter import filedialog
+import os
+from tkinter.ttk import Combobox
+import time
+
+
+folder_name = ""
 
 
 def choose_folder():
-    pass
+    global folder_name  # change to folder_name = function's return
+    folder_name = filedialog.askdirectory()
 
 
 def show_hidden_widgets():
-    pass
+
+    url_string = user_input.get()
+    url_video_object = YouTube(url_string)
+    title_label = Label(two_colums, text=url_video_object.title)
+
+    directory_button = Button(
+        three_colums, text="Choose folder", command=choose_folder)
+    save_button = Button(three_colums, text="Save video",
+                         command=download_video)
+
+    step_1 = Label(three_colums, text="1. Choose quality:")
+    step_2 = Label(three_colums, text="2. Saving folder:")
+    step_3 = Label(three_colums, text="3. Download:")
+    step_1.grid(row=0, column=0, sticky=W+E)
+    step_2.grid(row=0, column=1, sticky=W+E)
+    step_3.grid(row=0, column=2, sticky=W+E)
+
+    title_label.grid(sticky="W", row=1, column=0, pady=10)
+    choose_quality.grid(row=2, column=0, sticky=W+E)
+    directory_button.grid(row=2, column=1, sticky=W+E)
+    save_button.grid(row=2, column=2, sticky=W+E)
 
 
 # Configuring the main window
@@ -35,7 +63,7 @@ user_input = Entry(two_colums)
 user_input.insert(0, "Paste video URL here")
 user_input.configure(state=DISABLED)
 start_button = Button(two_colums, text="Analyze", command=show_hidden_widgets)
-choose_quality = Combobox(three_colums, values = ['360p' , '480p' , '720p'])
+choose_quality = Combobox(three_colums, values=['360p', '480p', '720p'])  # ugly, breakable
 two_colums.pack(fill="x", padx=20, pady=10)
 three_colums.pack(fill="x", padx=20, pady=20)
 
@@ -43,6 +71,11 @@ three_colums.pack(fill="x", padx=20, pady=20)
 # Putting our widgets into correct positions
 user_input.grid(row=0, column=0, sticky=W+E)
 start_button.grid(row=0, column=1, sticky=W+E)
+
+
+# Adding copyright warning
+warning_message = Label(window, text="WARNING! Some YouTube videos have copyright restrictions.\nDownloading copyrighted videos without permission is a criminal act.\nYou are prohibited for any illegal use of this app.\nAlways check for video copyright and YouTube's Terms of Service.\n\nPlease don't sue me :)", foreground="red")
+warning_message.place(relx=.5, y=240, anchor="center")
 
 
 def on_click(event):
